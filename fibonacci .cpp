c3f103e8a81294361648c175e9619a9686ce2e38
bassant2005 <basant.tareq.2005@gmail.com>
2025-10-26 15:32:22 +0300

    iterative binary search & recursive , dp Fibonacci

--- fibonacci .cpp
+++ fibonacci .cpp
@@ -1,3 +1,48 @@
-//
-// Created by bassant&yousef on 26/10/2025.
-//
+#include <iostream>
+using namespace std;
+
+// ===== function to print Fibonacci series using any function =====
+void PrintFibonacciSeries(int n, int (*f)(int)) {
+    for (int i = 0; i < n; i++)
+        cout << f(i) << " ";
+    cout << endl;
+}
+
+// ===== Recursive function =====
+int RecursiveFibonacci(int n) {
+    if (n <= 1)
+        return n;
+    return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2);
+}
+
+// ===== Dynamic Programming function =====
+int DynamicProgrammingFibonacci(int n) {
+    if (n <= 1)
+        return n;
+
+    int dp[n + 1];
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: " ;
+    cin >> n;
+    cout << endl;
+
+    cout << "Fibonacci Series (Recursive): ";
+    PrintFibonacciSeries(n, RecursiveFibonacci);
+    cout << "Fibonacci " << n << " : " << RecursiveFibonacci(n) << endl;
+
+    cout << endl;
+
+    cout << "Fibonacci Series (DP): ";
+    PrintFibonacciSeries(n, DynamicProgrammingFibonacci);
+    cout << "Fibonacci " << n << " : " << DynamicProgrammingFibonacci(n) << endl;
+}
--- search.cpp
+++ search.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
 
+// ===== Helper Function =====
+void performSearch(int arr[], int size, int target,
+                   int (*searchFunc)(int[], int, int)) {
+
+    int result = searchFunc(arr, size, target);
+
+    if (result != -1)
+        cout << "Element " << target << " found at index " << result << "\n\n";
+    else
+        cout << "Element " << target << " not found.\n\n";
+}
+
+// ===== Binary Search Function =====
+int binarySearch(int arr[], int size, int target) {
+    int left = 0, right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+
+// ===== Main Function =====
+int main() {
+    int arr[] = {2, 4, 6, 8, 10, 12, 14};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target;
+
+    cout << "Enter a number to search: ";
+    cin >> target;
+
+    cout << "Binary Search result: ";
+    performSearch(arr, size, target, binarySearch);
+}
